7f9e3f4c29871ced7651e1f54ad2f18c71d6b859
Hamza Haroon <hamzaharoon1314@users.noreply.github.com>
2023-04-25 13:18:53 +0500

    1.0.7

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,28 @@
 function stripBackgroundColor(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const elements = doc.querySelectorAll('*');
-  
-    elements.forEach(element => {
-      if (element.style.backgroundColor) {
-        element.style.backgroundColor = '';
-      }
-    });
-  
-    return doc.body.innerHTML;
-  }
-  
-  document.addEventListener('copy', (event) => {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const clonedContent = range.cloneContents();
-  
-    const container = document.createElement('div');
-    container.appendChild(clonedContent);
-  
-    const strippedHtml = stripBackgroundColor(container.innerHTML);
-    event.clipboardData.setData('text/html', strippedHtml);
-    event.preventDefault();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const elements = doc.querySelectorAll('*');
+
+  elements.forEach(element => {
+    if (element.style.backgroundColor) {
+      element.style.backgroundColor = '';
+    }
   });
-  
+
+  return doc.body.innerHTML;
+}
+
+document.addEventListener('copy', (event) => {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const clonedContent = range.cloneContents();
+
+  const container = document.createElement('div');
+  container.appendChild(clonedContent);
+
+  const strippedHtml = stripBackgroundColor(container.innerHTML);
+  const plainText = selection.toString();
+  event.clipboardData.setData('text/html', strippedHtml);
+  event.clipboardData.setData('text/plain', plainText);
+  event.preventDefault();
+});
